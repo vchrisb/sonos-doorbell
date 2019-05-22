ec727a3e9e9be756a4cdf630e64c93eece49978c
vchrisb <vchrisb@users.noreply.github.com>
2019-05-22 13:34:41 +0200

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -20,20 +20,6 @@ platforms=${platforms::-1}
 buildctl build --frontend dockerfile.v0 \
       --local dockerfile=. \
       --local context=. \
-      --output type=image,name=docker.io/$DOCKER_USER/sonos-doorbell:test-build,push=true \
+      --output type=image,name=docker.io/$DOCKER_USERNAME/sonos-doorbell:test-build,push=true \
       --opt platform=$platforms \
       --opt filename=./Dockerfile.cross
-
-# Push image for every arch with arch prefix in tag
-for arch in $architectures
-do
-# Build for all architectures and push manifest
-  buildctl build --frontend dockerfile.v0 \
-      --local dockerfile=. \
-      --local context=. \
-      --exporter image \
-      --exporter-opt name=docker.io/$DOCKER_USER/sonos-doorbell:test-build-$arch \
-      --exporter-opt push=true \
-      --frontend-opt platform=linux/$arch \
-      --frontend-opt filename=./Dockerfile.cross &
-done
